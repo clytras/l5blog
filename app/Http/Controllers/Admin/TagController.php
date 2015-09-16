799feebe39748c71bc3f4a30f4a891d61a61f44d
Christos Lytras <christos.lytras@gmail.com>
2015-09-17 00:32:12 +0300

    Chapter 8 commit

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TagCreateRequest;
+use App\Http\Requests\TagUpdateRequest;
+use App\Tag;
 
 class TagController extends Controller
 {
+    protected $fields = [
+        'tag' => '',
+        'title' => '',
+        'subtitle' => '',
+        'meta_description' => '',
+        'page_image' => '',
+        'layout' => 'blog.layouts.index',
+        'reverse_direction' => 0,
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -16,7 +28,10 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        $tags = Tag::all();
+        
+        return view('admin.tag.index')
+            ->withTags($tags);
     }
 
     /**
@@ -26,7 +41,12 @@ class TagController extends Controller
      */
     public function create()
     {
-        //
+        $data = [];
+        foreach($this->fields as $field => $default) {
+            $data[$field] = old($field, $default);
+        }
+    
+        return view('admin.tag.create', $data);
     }
 
     /**
@@ -35,20 +55,16 @@ class TagController extends Controller
      * @param  Request  $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store(TagCreateRequest $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
+        $tag = new Tag();
+        foreach(array_keys($this->fields) as $field) {
+            $tag->$field = $request->get($field);
+        }
+        $tag->save();
+    
+        return redirect('/admin/tag')
+            ->withSuccess("The tag '$tag->tag' was created.");
     }
 
     /**
@@ -59,7 +75,13 @@ class TagController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+        $data = ['id' => $id];
+        foreach(array_keys($this->fields) as $field) {
+            $data[$field] = old($field, $tag->$field);
+        }
+        
+        return view('admin.tag.edit', $data);
     }
 
     /**
@@ -69,9 +91,21 @@ class TagController extends Controller
      * @param  int  $id
      * @return Response
      */
-    public function update(Request $request, $id)
+    public function update(TagUpdateRequest $request, $id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+        
+        foreach(array_keys(array_except($this->fields, ['tag'])) as $field) {
+            $tag->$field = $request->get($field);
+        }
+        $tag->save();
+        
+        if($request->get('close') == 'true')
+            $ret = redirect("/admin/tag");
+        else 
+            $ret = redirect("/admin/tag/$id/edit");
+        
+        return $ret->withSuccess("Changes saved.");
     }
 
     /**
@@ -82,6 +116,10 @@ class TagController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+        $tag->delete();
+        
+        return redirect('/admin/tag')
+            ->withSuccess("The '$tag->tag' tag has been deleted.");
     }
 }
