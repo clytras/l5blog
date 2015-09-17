4273d8b92f202828605c322f5eed5f67ddee7d16
Christos Lytras <christos.lytras@gmail.com>
2015-09-17 16:57:32 +0300

    Chapter 9 commit (No Amazon S3 integration)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,7 +30,12 @@ $router->group([
 ], function () {
     Route::resource('admin/post', 'PostController');
     Route::resource('admin/tag', 'TagController', ['except' => 'show']);
+
     Route::get('admin/upload', 'UploadController@index');
+    Route::post('admin/upload/file', 'UploadController@uploadFile');
+    Route::delete('admin/upload/file', 'UploadController@deleteFile');
+    Route::post('admin/upload/folder', 'UploadController@createFolder');
+    Route::delete('admin/upload/folder', 'UploadController@deleteFolder');
 });
 
 // Logging in and out
